a3ae114a4380da7a73c50b1f946961dd7afa01f8
Anya Chidiebere <anyachidiebere029@gmail.com>
2024-10-28 17:52:12 +0100

    Updating routes and adding pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
+import { BrowserRouter as Router, Route, Routes, Switch } from 'react-router-dom';
+import Create from '../pages/Create';
 
 
 function App ()
 {
   return (
     <>
-      <Navbar />
-      <Home />
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" element={<Home />} />
+        </Switch>
+      </Router>
     </>
   );
 }
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,14 +1,15 @@
 import { BrowserRouter as Router, Route, Routes, Switch } from 'react-router-dom';
 import Home from '../pages/Home';
 // import PostDetail from '../pages/PostDetail';
-// import About from '../pages/About';
+import Create from '../pages/Create';
 
 const AppRoutes = () => (
   <Router>
     {/* <Routes> */}
       <Route path="/" element={<Home />} />
       {/* <Route path="/post/:id" element={<PostDetail />} /> */}
-      {/* <Route path="/about" element={<About />} />
+      <Route path="/create" element={<Create />} />
+      <Route path="/create" element={<Create />} />
     {/* </Routes> */}
   </Router>
 );
